0b5e29db7aae8bf574fa0b2d757e4a8a0b1ab5b2
azizmn12 <azizmnaffakh12@gmail.com>
2019-08-28 17:12:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style2.css';
+import img1 from "./4.jpg";
+
+
+  
+    
+<div >
+
+ <h1 className="title red">My title</h1>
+
+<br/>
+
+<img src={img1}/>
+
+<br/>
+
+<img src="/5.jpg/"/>
+</div>
+export default App;
+
+
+{/* // <div className="App">
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
+// </div> */}
